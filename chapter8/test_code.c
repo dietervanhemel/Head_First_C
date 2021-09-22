2d0571383aa427a23272b802e0aaa4c61d9f1cc7
dietervanhemel <dieter_vanhemel@hotmail.com>
2021-09-22 10:55:32 +0200

    header, objects and archive files

--- chapter8/test_code.c
+++ chapter8/test_code.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "encrypt.h"
-#include "checksum.h"
+#include "../my_header_files/encrypt.h"
+#include "../my_header_files/checksum.h"
 
 int main(){
 	char s[] = "Speak friend and enter";
