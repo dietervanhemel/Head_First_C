bc2c3ffd64796eedb7a5e51a0d40280c570067ad
dietervanhemel <dieter_vanhemel@hotmail.com>
2021-09-24 12:19:50 +0200

    Update secret_messages.c

--- chapter3/secret/secret_messages.c
+++ chapter3/secret/secret_messages.c
@@ -4,7 +4,8 @@ int main()
 {
     char word[10];
     int i = 0;
-    while(scanf("%9s", word) == 1){
+    while(scanf("%9s", word) == 1)
+    {
         i = i + 1;
         if(i % 2)
             fprintf(stdout, "%s\n", word);
