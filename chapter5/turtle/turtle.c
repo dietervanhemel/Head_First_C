bfa4f39ca59127d48dc405f653601dd6ad50b077
dietervanhemel <dieter_vanhemel@hotmail.com>
2021-10-01 11:43:09 +0200

    pointer to structure
    Updating structure, with pointer to structure

---
+++ chapter5/turtle/turtle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct {
+    const char *name;
+    const char *species;
+    int age;
+}turtle;
+
+void happy_birthday(turtle *a)
+{
+    a->age = a->age + 1;
+    printf("Happy Birthday %s! You are now %i years old!\n", 
+        a->name, a->age);
+}
+
+int main()
+{
+    turtle myrtle = {"Myrtle", "Leatherback sea turtle", 99};
+    happy_birthday(&myrtle);
+    printf("%s's age is now %i\n", myrtle.name, myrtle.age);
+    return 0;
+}
