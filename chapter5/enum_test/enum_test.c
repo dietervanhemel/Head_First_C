cbf365a613ac5db7a9e665619f1ad13b644c9dad
dietervanhemel <dieter_vanhemel@hotmail.com>
2021-10-01 13:10:59 +0200

    enum, union, struct, typedef

--- chapter5/enum_test/enum_test.c
+++ chapter5/enum_test/enum_test.c
@@ -21,18 +21,18 @@ void display(fruit_order order)
 {
     printf("This order contains ");
     if(order.units == PINTS)
-        printf("%2.2f pints of %s\n", order.amount.count, order.name);
+        printf("%2.2f pints of %s\n", order.amount.volume, order.name);
     else if(order.units == POUNDS)
         printf("%2.2f lbs of %s\n", order.amount.weight, order.name);
     else
-        printf("%i %s\n", order.amount.volume, order.name);
+        printf("%i %s\n", order.amount.count, order.name);
 }
 
 int main()
 {
     fruit_order apples = {"apples", "England", .amount.count=144, COUNT};
     fruit_order strawberries = {"strawberries", "Spain", .amount.weight=17.6, POUNDS};
-    fruit_order oj = {"orange juice", "U.S.A.", .amount.volume, PINTS};
+    fruit_order oj = {"orange juice", "U.S.A.", .amount.volume=10.5, PINTS};
 
     display(apples);
     display(strawberries);
