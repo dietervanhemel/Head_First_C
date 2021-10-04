c5245331a055d6ef9d69f6cc986903930f1d65c2
dietervanhemel <dieter_vanhemel@hotmail.com>
2021-10-04 13:50:34 +0200

    command-line arguments - argc & argv[] - error detection
    Added some error detection for opening file "spooky.csv"
    command-line arguments - argc & argv[]

--- chapter3/categorize/categorize.c
+++ chapter3/categorize/categorize.c
@@ -11,7 +11,11 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    FILE *in = fopen("spooky.csv", "r");
+    FILE *in;
+    if(!(in = fopen("spooky.csv", "r"))){
+        fprintf(stderr, "Can't open the file.\n");
+        return 1;
+    }
 
     FILE *file1 = fopen(argv[2], "w");
 
